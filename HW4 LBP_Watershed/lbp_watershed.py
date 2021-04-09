ad474fb7d587655fe899de34c67ae01b51b00d5a
Sapphire1002 <62550868+Sapphire1002@users.noreply.github.com>
2021-04-09 14:30:17 +0800

    Embbeded

--- HW4 LBP_Watershed/lbp_watershed.py
+++ HW4 LBP_Watershed/lbp_watershed.py
@@ -4,41 +4,6 @@ import matplotlib.pyplot as plt
 
 
 def compare(img, sample, high=32, width=8, hist=False, condi=0.5):
-    """
-    cv2 方法參數說明
-    calcHist(images, channels, mask, histSize, ranges[, hist[, accumulate]]) -> hist
-    images: 輸入圖片
-    channels: 灰階則使用[0], RGB通道有三個[0, 1, 2] -> [B, G, R]
-    mask: None代表計算整張圖片, 否則可以指定想要處理的特定位置
-    histSize: Histogram 大小
-    ranges: 直方圖 x軸範圍
-
-
-    normalize(src, [, dst[, alpha[, beta[, norm_type[, dtype[, mask]]]]]]) -> dst
-    src: 輸入數組
-    dst: 和 src 大小相同的輸出數組
-    alpha: 標準化下限
-    beta: 標準化上限
-    norm_type: 範數類型
-    dtype: 若為負, 則輸出數組有和 src 相同類型, 否則有與 src 相同的通道數和深度
-
-    4 種 norm_type
-    1. NORM_MINMAX: 線性歸一化
-    2. NORM_INF: 切比雪夫距離
-    3. NORM_L1: 曼哈頓距離(L1-norm 絕對值的和)
-    4. NORM_L2: 歐幾里得距離(L2-norm 平方開根號的和)
-
-
-    compareHist(hist1, hist2, method)
-    hist1, hist2: 兩張要比較的直方圖
-    method: 比較方式
-
-    3 種比較方式
-    HISTCMP_CORREL: 相關性比較, 值越大, 相關度越高, 區間 [0, 1]
-    HISTCMP_CHISQR: 卡方比較, 值越小, 相關度越高, 區間 [0, ∞)
-    HISTCMP_BHATTACHARYYA: 巴氏距離比較, 值越小, 相關度越高, 區間 [0, 1]
-    """
-
     # 儲存指定區塊的sample
     y, x = sample
     s = img[y:y+high, x:x+width]
@@ -46,12 +11,8 @@ def compare(img, sample, high=32, width=8, hist=False, condi=0.5):
     # 統計 sample 的 LBP histogram feature vector
     grayscale, count = np.unique(s, return_counts=True)
 
-    # 使用 calcHist() 取得直方圖數據[標準化]
+    # 使用 calcHist() 取得直方圖數據
     s_hist = cv2.calcHist([s], [0], None, [256], [0, 255])
-    # s_hist = cv2.normalize(s_hist, s_hist, 0, 1, cv2.NORM_MINMAX, -1)
-
-    # sim = cv2.compareHist(s_hist, s_hist, cv2.HISTCMP_CHISQR)
-    # print(sim)
 
     # 和圖片的每個 high*width 做比較(採用 相關性比較)
     # 將相似度符合的條件畫成同個 color
@@ -145,8 +106,6 @@ unknown = cv2.subtract(bg, fg)
 _, markers = cv2.connectedComponents(fg)
 print(np.unique(markers, return_counts=True))
 markers = cv2.watershed(road, markers=markers)
-# markers = markers + 1
-# markers[unknown == 255] = 0
 road[markers == -1] = [0, 0, 0]
 # cv2.imshow("watershed", road)
 
@@ -162,6 +121,5 @@ color_block[markers == 3] = [152, 251, 152]
 result = cv2.addWeighted(road, 1, color_block, 0.8, 0)
 
 cv2.imshow("result", result)
-# cv2.imwrite("./road/road_lbp_watershed.png", result)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
